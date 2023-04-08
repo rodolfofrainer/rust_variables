ac342a63141d11c8b409dd8838c01a301930fc05
Rodolfo Frainer <91473305+rodolfofrainer@users.noreply.github.com>
2023-04-08 15:25:54 +0100

    practice different types of variables

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x=5;
+
+    println!("The value of x is: {}", x);
+
+    x = 13;
+    println!("The value of x is: {}", x);
+
+    const SECONDS: i8 = 60;
+    println!("The value of SECONDS is: {}", SECONDS);
+}
